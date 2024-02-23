9b80a2e7b37df996e522b9c68b68cb1840b00772
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-02-22 21:44:02 -0800

    mocking api

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,9 +1,17 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
+import tags from '../test-data/tags.json'
 
 test.beforeEach(async ({ page }) => {
+    await page.route('*/**/api/tags', async route => {
+        await route.fulfill({
+            body: JSON.stringify(tags)
+        })
+    })
+
+    // await page.waitForTimeout(1000);
     await page.goto('https://conduit.bondaracademy.com/')
 })
 
 test('has title', async ({ page }) => {
-    await expect(page.locator('.navbar-brand')).toHaveText('conduit');
-});
+    await expect(page.locator('.navbar-brand')).toHaveText('conduit')
+})
