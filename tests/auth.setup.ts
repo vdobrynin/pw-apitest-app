84b32ca1587fc89184d315958280547ac056297f
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-02-28 22:41:41 -0800

    api authentication

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,14 +1,23 @@
 import { test as setup } from '@playwright/test';
+import user from '../.auth/user.json'
+import fs from 'fs'
 
 const authFile = '.auth/user.json'
 
-setup('authentication', async ({ page }) => {
-    await page.goto('https://conduit.bondaracademy.com/');
-    await page.getByText(' Sign in ').click()
-    await page.getByRole('textbox', { name: 'Email' }).fill('pwtest60@test.com')
-    await page.getByRole('textbox', { name: 'Password' }).fill('vd12345')
-    await page.getByRole('button').click()
-    await page.waitForResponse('https://conduit-api.bondaracademy.com/api/tags')
+setup('authentication', async ({ request }) => {
 
-    await page.context().storageState({ path: authFile })
+    const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
+        data: {
+            "user": { email: "pwtest60@test.com", password: "vd12345" }
+        }
+    })
+
+    const responseBody = await response.json()
+    const accessToken = responseBody.user.token
+
+    user.origins[0].localStorage[0].value = accessToken
+    fs.writeFileSync(authFile, JSON.stringify(user))
+
+    process.env['ACCESS_TOKEN'] = accessToken
+
 })
--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -35,22 +35,10 @@ test('has title', async ({ page }) => {
 
 test('delete the article', async ({ page, request }) => {
 
-    const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
-        data: {
-            "user": { email: "pwtest60@test.com", password: "vd12345" }
-        }
-    })
-
-    const responseBody = await response.json()
-    const accessToken = responseBody.user.token
-
     const articleResponse = await request.post('https://conduit-api.bondaracademy.com/api/articles/', {
         data: {
             "article": { "title": "This is a test article", "description": "This is a test description", "body": "This is a test body", "tagList": [] }
         },
-        headers: {
-            Authorization: `Token ${accessToken}`
-        }
     })
     expect(articleResponse.status()).toEqual(201)
 
@@ -82,20 +70,7 @@ test('create article', async ({ page, request }) => {
 
     await expect(page.locator('app-article-list h1').first()).toContainText('Playwright is awesome')
 
-    const response = await request.post('https://conduit-api.bondaracademy.com/api/users/login', {
-        data: {
-            "user": { email: "pwtest60@test.com", password: "vd12345" }
-        }
-    })
-
-    const responseBody = await response.json()
-    const accessToken = responseBody.user.token
-
-    const deleteArticleRequest = await request.delete(`https://conduit-api.bondaracademy.com/api/articles/${slugId}`,
-        {
-            headers: {
-                Authorization: `Token ${accessToken}`
-            }
-        })
+    const deleteArticleRequest = await request
+        .delete(`https://conduit-api.bondaracademy.com/api/articles/${slugId}`)
     expect(deleteArticleRequest.status()).toEqual(204)
 })
