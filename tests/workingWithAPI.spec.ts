ee2d3f059918a6384d9ace20708e32dd11664576
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-06-09 21:08:29 -0700

    #54 setup api project - review

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -1,36 +1,34 @@
 import { test, expect, request } from '@playwright/test';
-import tags from '../test-data/tags.json'
+// import tags from '../test-data/tags.json'
 
 test.beforeEach(async ({ page }) => {
-
-    await page.route('*/**/api/tags', async route => {
-        await route.fulfill({
-            body: JSON.stringify(tags)
-        })
-    })
+    // await page.route('*/**/api/tags', async route => {
+    //     await route.fulfill({
+    //         body: JSON.stringify(tags)
+    //     })
+    // })
 
     await page.goto('https://conduit.bondaracademy.com/');
 })
 
 test('has title', async ({ page }) => {
+    // await page.waitForTimeout(50)
+    // await page.route('*/**/api/articles*', async route => {
+    //     const response = await route.fetch()                 // storing response in variable
+    //     const responseBody = await response.json()
+    //     responseBody.articles[0].title = 'This is a MOCK test title'
+    //     responseBody.articles[0].description = 'This is a MOCK description'
 
-    await page.waitForTimeout(50)
-    await page.route('*/**/api/articles*', async route => {
-        const response = await route.fetch()                 // storing response in variable
-        const responseBody = await response.json()
-        responseBody.articles[0].title = 'This is a MOCK test title'
-        responseBody.articles[0].description = 'This is a MOCK description'
+    //     await route.fulfill({
+    //         body: JSON.stringify(responseBody)
+    //     })
+    // })
 
-        await route.fulfill({
-            body: JSON.stringify(responseBody)
-        })
-    })
-
-    await page.getByText('Global Feed').click()
+    // await page.getByText('Global Feed').click()
     await expect(page.locator('.navbar-brand')).toHaveText('conduit')
-    await expect(page.locator('app-article-list h1').first()).toContainText('This is a MOCK test title')
-    await expect(page.locator('app-article-list p').first()).toContainText('This is a MOCK description')
-    // await page.waitForTimeout(100) // if assertion not present we need to wait some time
+    // await expect(page.locator('app-article-list h1').first()).toContainText('This is a MOCK test title')
+    // await expect(page.locator('app-article-list p').first()).toContainText('This is a MOCK description')
+    // // await page.waitForTimeout(100) // if assertion not present we need to wait some time
 })
 
 test('delete the article', async ({ page, request }) => {
