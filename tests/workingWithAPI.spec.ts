0d7848e8509a6b0935eba431318c3ea110ca88b5
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-06-10 18:13:43 -0700

    #57 perform api request 1st part - review

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -2,48 +2,46 @@ import { test, expect, request } from '@playwright/test';
 import tags from '../test-data/tags.json'
 
 test.beforeEach(async ({ page }) => {
-    await page.route('*/**/api/tags', async route => {  //#55 mocking API
+    await page.route('*/**/api/tags', async route => {  // #55 mocking API
         await route.fulfill({
             body: JSON.stringify(tags)
         })
     })
+    // await page.route('*/**/api/articles*', async route => { //  #57 remove & move below //intercept response in variable
+    //     const response = await route.fetch()                 // storing response in variable
+    //     const responseBody = await response.json()           // storing in json
+    //     responseBody.articles[0].title = 'This is a test title'  //modify of 1st title
+    //     responseBody.articles[0].description = 'This is a test description' //modify of description
 
-    await page.route('*/**/api/articles*', async route => {  // intercept response
-        const response = await route.fetch()                 // storing response in variable
-        const responseBody = await response.json()           // storing in json
-        responseBody.articles[0].title = 'This is a test title'  //modify of 1st title
-        responseBody.articles[0].description = 'This is a description' //modify of description
-
-        await route.fulfill({
-            body: JSON.stringify(responseBody)  // fulfill body response
-        })
-    })
-
+    //     await route.fulfill({
+    //         body: JSON.stringify(responseBody)  // fulfill body response
+    //     })
+    // })
     await page.goto('https://conduit.bondaracademy.com/');
 })
 
 test('has title', async ({ page }) => {
-    // await page.waitForTimeout(50)
-    // await page.route('*/**/api/articles*', async route => {
-    //     const response = await route.fetch()                 // storing response in variable
-    //     const responseBody = await response.json()
-    //     responseBody.articles[0].title = 'This is a MOCK test title'
-    //     responseBody.articles[0].description = 'This is a MOCK description'
+    await page.route('*/**/api/articles*', async route => { // #57 move in from above & change with MOCK
+        const response = await route.fetch()                 // storing response in variable
+        const responseBody = await response.json()           // storing in json
+        responseBody.articles[0].title = 'This is a MOCK test title'        //modify of MOCK title
+        responseBody.articles[0].description = 'This is a MOCK description' //modify of MOCK description
+        // responseBody.articles[0].title = 'This is a test title'              //modify of 1st title
+        // responseBody.articles[0].description = 'This is a test description'  //modify of 1st description
 
-    //     await route.fulfill({
-    //         body: JSON.stringify(responseBody)
-    //     })
-    // })
+        await route.fulfill({
+            body: JSON.stringify(responseBody)
+        })
+    })
 
-    // await page.getByText('Global Feed').click()
+    await page.getByText('Global Feed').click()
     await expect(page.locator('.navbar-brand')).toHaveText('conduit') // assertion for page
-    await expect(page.locator('app-article-list h1').first()).toContainText('This is a test title') //assertions for test
-    await expect(page.locator('app-article-list p').first()).toContainText('This is a description')
-    // // await page.waitForTimeout(1000) // if assertion not present we need to wait some time
+    await expect(page.locator('app-article-list h1').first()).toContainText('This is a MOCK test title') //assertions for test
+    await expect(page.locator('app-article-list p').first()).toContainText('This is a MOCK description')
+    await page.waitForTimeout(1000) //if assertion not present we need to wait *(bug in playwright)*
 })
 
 test('delete the article', async ({ page, request }) => {
-
     const articleResponse = await request.post('https://conduit-api.bondaracademy.com/api/articles/', {
         data: {
             "article": { "title": "This is a test title", "description": "This is a test description", "body": "This is a test body", "tagList": [] }
