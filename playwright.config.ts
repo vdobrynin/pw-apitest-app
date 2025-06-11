b325e0acc1548ba1da1eab3ad6c7e14242982c9f
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2025-06-10 17:07:02 -0700

    #54 setup api project - review

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,6 @@ export default defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-    baseURL: 'http://localhost:4200/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -33,8 +32,8 @@ export default defineConfig({
       'Authorization': `Token ${process.env.ACCESS_TOKEN}`
     }
   },
-  globalSetup: require.resolve('./global-setup.ts'),        // ---> global setup
-  globalTeardown: require.resolve('./global-teardown.ts'),   // ---> global teardown
+  // globalSetup: require.resolve('./global-setup.ts'),        // ---> global setup
+  // globalTeardown: require.resolve('./global-teardown.ts'),   // ---> global teardown
 
   /* Configure projects for major browsers */
   projects: [
@@ -99,10 +98,10 @@ export default defineConfig({
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
     // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    },
   ],
 
   /* Run your local dev server before starting the tests */
